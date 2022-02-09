dc4ea55f630e052a1a15c89f82a2108e065bd4c1
Spinu Gheorghe <gheorghespinu2001@gmail.com>
2022-02-09 17:22:30 +0200

    Initial commit

---
+++ ConsollAPP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ConsollAPP.BinaryGap;
+
+namespace ConsollAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Raspunsul pentru task-ul nr.1
+            int result1 = Task1.task1(1041);
+            Console.WriteLine("Rezultatul task1: " + result1);
+        }
+    }
+}
