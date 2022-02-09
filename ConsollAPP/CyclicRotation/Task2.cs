42f923aec77724a01d47e82ec99611ac3aa50fe5
Spinu Gheorghe <gheorghespinu2001@gmail.com>
2022-02-09 18:00:55 +0200

    Task_2

---
+++ ConsollAPP/CyclicRotation/Task2.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsollAPP.CyclicRotation
+{
+    public class Task2
+    {
+        public static int[] solution(int[] A, int K)
+        {
+            
+            if(A.Length == 0 || K == 0)
+            {
+                return A;
+            }
+
+            int[] B = new int[A.Length];
+
+            B[0] = A[A.Length - 1];
+
+            for (int j = 0; j < A.Length - 1; j++)
+            {
+                B[j + 1] = A[j];
+            }
+
+            if (K > 1)
+            {
+                return solution(B, K - 1);
+            }
+            else
+            {
+                return B;
+            }
+        } 
+    }
+}
--- ConsollAPP/Program.cs
+++ ConsollAPP/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ConsollAPP.BinaryGap;
+using ConsollAPP.CyclicRotation;
 
 namespace ConsollAPP
 {
@@ -11,6 +12,18 @@ namespace ConsollAPP
             //Raspunsul pentru task-ul nr.1
             int result1 = Task1.task1(1041);
             Console.WriteLine("Rezultatul task1: " + result1);
+            
+            // Raspunsul pentru task-ul nr.2
+             int[]  A = {3, 8, 9, 7, 6};
+
+             Console.Write("Our String is: \n");
+             for (int i = 0 ; i < A.Length; i++) 
+             {
+                 Console.Write(A[i] + " ");
+             }
+            
+             int[] result2 = Task2.solution(A, 3);
+            Console.WriteLine(result2);
         }
     }
 }
