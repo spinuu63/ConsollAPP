13b5c9ecbcc0c623752011e3cfbb4ec09344512f
Spinu Gheorghe <gheorghespinu2001@gmail.com>
2022-02-09 18:41:29 +0200

    test

---
+++ ConsollAPP/PassingCars/Task11.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsollAPP.PassingCars
+{
+    public class Task11
+    {
+        public static int solution(int[] A)
+        {
+            var lenght = A.Length;
+            if (lenght < 0 || lenght > 1000000) throw new ArgumentOutOfRangeException();
+            var lastcar = 0;
+            var newcar = 0;
+
+            foreach (var value  in A)
+            {
+                if (value == 1)
+                {
+                    newcar += lastcar;
+                    if (newcar > 1000000000) return -1;
+                }
+                else
+                    lastcar++;
+            }
+
+            return newcar;
+        }
+
+    }
+}
